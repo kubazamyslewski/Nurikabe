0169d6b8347a13a64d433e1a2a5dd70709b2a801
Jakub Polak <jakub.polak4.stud@pw.edu.pl>
2024-01-07 14:40:16 +0100

    Choosing difficulty and assigning size of the board to it

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class Game {
+    static int size;
+    public static void main(String[]args){
+        size = difficultyLevel();
+        Generator generator = new Generator(size);
+
+    }
+
+    static int difficultyLevel(){
+        System.out.println("Wybierz poziom trudności:");
+        System.out.println("1. Łatwy");
+        System.out.println("2. Średni");
+        System.out.println("3. Trudny");
+
+        Scanner scanner = new Scanner(System.in);
+        int temp = scanner.nextInt();
+        scanner.close();
+
+        switch(temp){
+            case 1: return 5;
+            case 2: return 7;
+            case 3: return 10;
+            default: System.out.println("Źle wybrany numer");
+        }
+        return 0;
+    }
 }
