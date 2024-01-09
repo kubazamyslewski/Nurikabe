c6982b88eda07e73741f73895d51ed0801ddd4c8
Jakub Polak <jakub.polak4.stud@pw.edu.pl>
2024-01-09 13:20:49 +0100

    First try of GUI

--- src/GameInterface.java
+++ src/GameInterface.java
@@ -1,2 +1,66 @@
+import javax.swing.*;
+
 public class GameInterface {
+
+    static int selectDifficulty(){
+        Object[] options = {"Trudny", "Średni", "Łatwy"};
+
+        // Wyświetlanie okna dialogowego z trzema opcjami
+        int selectedOption = JOptionPane.showOptionDialog(
+                null,
+                "Wybierz jedną z opcji:",
+                "Okno z Opcjami",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null,
+                options,
+                options[0]
+        );
+
+        int level = 0;
+        // Obsługa wybranej opcji
+        if (selectedOption == 0) {
+            System.out.println("Wybrano poziom trudny");
+            level = 3;
+        } else if (selectedOption == 1) {
+            System.out.println("Wybrano poziom średni");
+            level = 2;
+        } else if (selectedOption == 2) {
+            System.out.println("Wybrano poziom łatwy");
+            level = 1;
+        } else {
+            System.out.println("Nie wybrano żadnej opcji");
+        }
+        return level;
+    }
+
+    static int whichBoard(){
+        Object[] options = {"Wygeneruj planszę", "Wczytaj planszę"};
+
+        // Wyświetlanie okna dialogowego z trzema opcjami
+        int selectedOption = JOptionPane.showOptionDialog(
+                null,
+                "Wybierz jedną z opcji:",
+                "Okno z Opcjami",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null,
+                options,
+                options[0]
+        );
+
+        int option = 0;
+        // Obsługa wybranej opcji
+        if (selectedOption == 0) {
+            System.out.println("Generuję planszę");
+            option = 1;
+        } else if (selectedOption == 1) {
+            System.out.println("Wczytuję planszę");
+            option = 2;
+        } else {
+            System.out.println("Nie wybrano żadnej opcji");
+        }
+        return option;
+    }
+
 }
