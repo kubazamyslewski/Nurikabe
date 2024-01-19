e4a0c8f4465aea1ec40fd54fe9934f22fa076b8d
norbe <01187006@pw.edu.pl>
2024-01-20 00:05:40 +0100

    GUI z kolorowaniem kwadratów, gdy jest niedozwolony ruch

--- src/GameInterface.java
+++ src/GameInterface.java
@@ -5,27 +5,22 @@ import java.awt.event.ActionListener;
 
 public class GameInterface extends JFrame {
 
-    private  final int SIZE;  // Rozmiar planszy
+    private final int SIZE;  // Rozmiar planszy
     private JButton[][] buttons;  // Tablica przycisków reprezentujących komórki planszy
     private int[][] intBoard;
 
     public GameInterface(int[][] intBoard) {
-        //Generator generator = new Generator();
         this.intBoard = intBoard;
         this.SIZE = intBoard.length;
-        // Konfiguracja głównego okna JFrame
         setTitle("Gra Nurikabe");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(SIZE, SIZE));
         setResizable(false);
 
-        // Inicjalizacja tablicy przycisków
         buttons = new JButton[SIZE][SIZE];
 
-        // Tworzenie przycisków i dodawanie ich do JFrame
         initializeButtons();
 
-        // Pakowanie i wyświetlanie JFrame
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
@@ -42,8 +37,7 @@ public class GameInterface extends JFrame {
                 buttons[i][j] = button;
 
                 if (intBoard[i][j] >= 0) {
-                    //button.setText(Integer.toString(intBoard[i][j]));
-                    updateButtonIcon(i, j);
+                    updateButtonIconOnStart(i, j);
                 }
 
                 //button.doClick(0);    // <- zabawny item
@@ -63,63 +57,141 @@ public class GameInterface extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            if (intBoard[row][col] == 0){
-                intBoard[row][col] = -1;
-            }
-            else if (intBoard[row][col] == -1){
-                intBoard[row][col] = 0;
-            }
+            intBoard[row][col] = -1 - intBoard[row][col];
             updateButtonIcon(row, col);
         }
     }
 
     private void updateButtonIcon(int row, int col) {
         int value = intBoard[row][col];
-        if (value == -1){
-            // Obsługa zdarzenia kliknięcia przycisku (możesz dostosować tę część)
-            String imagePath = "Pictures/" + "zamalowane" + ".jpg";  // Zakładamy, że obrazy mają nazwy image0.jpg, image1.jpg, itd.
-            ImageIcon originalIcon = new ImageIcon(getClass().getResource(imagePath));
-            Image originalImage = originalIcon.getImage();
+        String imagePath;
+        ImageIcon originalIcon;
+        Image originalImage;
+        int targetSize = 50;
+        Image scaledImage;
+        ImageIcon scaledIcon;
 
-            // Skalowanie obrazu
-            int targetSize = 50;  // Docelowy rozmiar przycisku
-            Image scaledImage = originalImage.getScaledInstance(targetSize, targetSize, Image.SCALE_SMOOTH);
+        if (value == -1) {
+            if (isItSquare(intBoard, row, col)) {
+                for (int i = -1; i < 2; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        if (isItSquare(intBoard, row + i, col + j) && intBoard[row + i][col + j] == -1) {
+                            imagePath = "Pictures/x.jpg";
+                            originalIcon = new ImageIcon(getClass().getResource(imagePath));
+                            originalImage = originalIcon.getImage();
+                            scaledImage = originalImage.getScaledInstance(targetSize, targetSize, Image.SCALE_SMOOTH);
+                            scaledIcon = new ImageIcon(scaledImage);
+                            buttons[row + i][col + j].setIcon(scaledIcon);
+                        }
+                    }
+                }
+            } else {
+                imagePath = "Pictures/zamalowane.jpg";
+                originalIcon = new ImageIcon(getClass().getResource(imagePath));
+                originalImage = originalIcon.getImage();
+                scaledImage = originalImage.getScaledInstance(targetSize, targetSize, Image.SCALE_SMOOTH);
+                scaledIcon = new ImageIcon(scaledImage);
 
-            // Utworzenie nowego ImageIcon ze skalowanym obrazem
-            ImageIcon scaledIcon = new ImageIcon(scaledImage);
+                buttons[row][col].setIcon(scaledIcon);
+            }
 
-            buttons[row][col].setIcon(scaledIcon);
         }
-        if (value == 0){
-            String imagePath = "Pictures/" + "puste" + ".jpg";  // Zakładamy, że obrazy mają nazwy image0.jpg, image1.jpg, itd.
-            ImageIcon originalIcon = new ImageIcon(getClass().getResource(imagePath));
-            Image originalImage = originalIcon.getImage();
+        if (value == 0) {
+            if (isItSquare(intBoard, row, col)) {
+                for (int i = -1; i < 2; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        if (!isItSquare(intBoard, row + i, col + j) && (row + i >= 0 && col + j >= 0 && row + i < SIZE && col + j < SIZE)) {
+                            if (intBoard[row + i][col + j] == -1) {
+                                imagePath = "Pictures/" + "zamalowane" + ".jpg";
+                                originalIcon = new ImageIcon(getClass().getResource(imagePath));
+                                originalImage = originalIcon.getImage();
+                                scaledImage = originalImage.getScaledInstance(targetSize, targetSize, Image.SCALE_SMOOTH);
+                                scaledIcon = new ImageIcon(scaledImage);
+                                buttons[row + i][col + j].setIcon(scaledIcon);
+                            }
+                        }
+                    }
+                }
 
-            // Skalowanie obrazu
-            int targetSize = 50;  // Docelowy rozmiar przycisku
-            Image scaledImage = originalImage.getScaledInstance(targetSize, targetSize, Image.SCALE_SMOOTH);
-
-            // Utworzenie nowego ImageIcon ze skalowanym obrazem
-            ImageIcon scaledIcon = new ImageIcon(scaledImage);
+            }
+            imagePath = "Pictures/puste.jpg";  // Zakładamy, że obrazy mają nazwy image0.jpg, image1.jpg, itd.
+            originalIcon = new ImageIcon(getClass().getResource(imagePath));
+            originalImage = originalIcon.getImage();
+            scaledImage = originalImage.getScaledInstance(targetSize, targetSize, Image.SCALE_SMOOTH);
+            scaledIcon = new ImageIcon(scaledImage);
 
             buttons[row][col].setIcon(scaledIcon);
         }
         if (value > 0) {
-            //String imagePath = "resources/" + value + ".jpg";
-            String imagePath = "Pictures/" + value + ".jpg";  // Zakładamy, że obrazy mają nazwy image0.jpg, image1.jpg, itd.
-            ImageIcon originalIcon = new ImageIcon(getClass().getResource(imagePath));
-            Image originalImage = originalIcon.getImage();
-
-            // Skalowanie obrazu
-            int targetSize = 50;  // Docelowy rozmiar przycisku
-            Image scaledImage = originalImage.getScaledInstance(targetSize, targetSize, Image.SCALE_SMOOTH);
-
-            // Utworzenie nowego ImageIcon ze skalowanym obrazem
-            ImageIcon scaledIcon = new ImageIcon(scaledImage);
+            imagePath = "Pictures/" + value + ".jpg";  // Zakładamy, że obrazy mają nazwy image0.jpg, image1.jpg, itd.
+            originalIcon = new ImageIcon(getClass().getResource(imagePath));
+            originalImage = originalIcon.getImage();
+            scaledImage = originalImage.getScaledInstance(targetSize, targetSize, Image.SCALE_SMOOTH);
+            scaledIcon = new ImageIcon(scaledImage);
 
             buttons[row][col].setIcon(scaledIcon);
         }
 
+    }
+
+
+    private void updateButtonIconOnStart(int row, int col) {
+        int value = intBoard[row][col];
+        String imagePath;
+        ImageIcon originalIcon;
+        Image originalImage;
+        int targetSize = 50;
+        Image scaledImage;
+        ImageIcon scaledIcon;
+
+        if (value == 0) {
+            imagePath = "Pictures/puste.jpg";  // Zakładamy, że obrazy mają nazwy image0.jpg, image1.jpg, itd.
+            originalIcon = new ImageIcon(getClass().getResource(imagePath));
+            originalImage = originalIcon.getImage();
+            scaledImage = originalImage.getScaledInstance(targetSize, targetSize, Image.SCALE_SMOOTH);
+            scaledIcon = new ImageIcon(scaledImage);
+
+            buttons[row][col].setIcon(scaledIcon);
+        }
+        if (value > 0) {
+            imagePath = "Pictures/" + value + ".jpg";  // Zakładamy, że obrazy mają nazwy image0.jpg, image1.jpg, itd.
+            originalIcon = new ImageIcon(getClass().getResource(imagePath));
+            originalImage = originalIcon.getImage();
+            scaledImage = originalImage.getScaledInstance(targetSize, targetSize, Image.SCALE_SMOOTH);
+            scaledIcon = new ImageIcon(scaledImage);
+
+            buttons[row][col].setIcon(scaledIcon);
+        }
+
+    }
+
+
+    boolean isItSquare(int[][] tempBoard, int x, int y) {
+        int size = tempBoard.length;
+        if (x < 0 || y < 0 || x >= size || y >= size) {
+            return false;
+        }
+        if (x - 1 >= 0 && y - 1 >= 0) {
+            if ((tempBoard[x - 1][y - 1] == -1) && (tempBoard[x][y - 1] == -1) && (tempBoard[x - 1][y] == -1)) {
+                return true;
+            }
+        }
+        if (x - 1 >= 0 && y + 1 < size) {
+            if ((tempBoard[x - 1][y + 1] == -1) && (tempBoard[x - 1][y] == -1) && (tempBoard[x][y + 1] == -1)) {
+                return true;
+            }
+        }
+        if (x + 1 < size && y + 1 < size) {
+            if ((tempBoard[x + 1][y + 1] == -1) && (tempBoard[x][y + 1] == -1) && (tempBoard[x + 1][y] == -1)) {
+                return true;
+            }
+        }
+        if (x + 1 < size && y - 1 >= 0) {
+            if ((tempBoard[x + 1][y - 1] == -1) && (tempBoard[x + 1][y] == -1) && (tempBoard[x][y - 1] == -1)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
@@ -183,5 +255,6 @@ public class GameInterface extends JFrame {
         }
         return option;
     }
+
 
 }
