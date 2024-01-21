9da5f36c3e43b8b616a9be0320f086ff4296ea3b
Mateusz Kowalczuk <jannkowalski@spoko.pln>
2024-01-21 23:06:45 +0100

    Testy jednostkowe solver poprawione

--- src/SolverTest.java
+++ src/SolverTest.java
@@ -5,37 +5,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class SolverTest {
 
     @Test
-    public void test_eazy_5boards() {
-        for(int i=0; i<5; ++i) {
-            Solver solver = new Solver();
-            Generator gen= new Generator();
-            int[][] boardToSolve =gen.makeBoardToPlay(gen.generateBoard(5));
-            int[][] solvedBoard = solver.solve(boardToSolve, 500000, 0);
-            Checker ch = new Checker();
-            assertTrue(ch.IsCorrect(solvedBoard));
-        }
+    public void test_eazy_board() {
+        Solver solver = new Solver();
+        Generator gen = new Generator();
+        int[][] boardToSolve = gen.makeBoardToPlay(gen.generateBoard(5));
+        int[][] solvedBoard = solver.solve(boardToSolve, 500000, 0);
+        Checker ch = new Checker();
+        assertTrue(ch.IsCorrect(solvedBoard));
     }
     @Test
-    public void test_medium_5boards() {
-        for(int i=0; i<5; ++i) {
+    public void test_medium_board() {
             Solver solver = new Solver();
             Generator gen= new Generator();
             int[][] boardToSolve =gen.makeBoardToPlay(gen.generateBoard(7));
             int[][] solvedBoard = solver.solve(boardToSolve, 500000, 0);
             Checker ch = new Checker();
            assertTrue(ch.IsCorrect(solvedBoard));
-        }
     }
     @Test
-    public void test_hard_5boards() {
-        for(int i=0; i<5; ++i) {
+    public void test_hard_board() {
             Solver solver = new Solver();
             Generator gen= new Generator();
             int[][] boardToSolve =gen.makeBoardToPlay(gen.generateBoard(10));
             int[][] solvedBoard = solver.solve(boardToSolve, 500000, 0);
             Checker ch = new Checker();
             assertTrue(ch.IsCorrect(solvedBoard));
-        }
     }
 
 }
